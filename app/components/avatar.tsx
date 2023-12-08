6e3c1635c4f52e6a04924c67d311b98cabf35267
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-08 13:18:41 +0000

    user status added

--- app/components/avatar.tsx
+++ app/components/avatar.tsx
@@ -17,6 +17,11 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
           fill
         />
       </div>
+      <span
+        role="status"
+        className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3"
+        aria-hidden
+      />
     </div>
   );
 };

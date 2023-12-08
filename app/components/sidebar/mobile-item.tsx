d5f54ddcf6a0a53afb1b2d13d03dca51ed6dd360
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-08 12:43:14 +0000

    typo fix

--- app/components/sidebar/mobile-item.tsx
+++ app/components/sidebar/mobile-item.tsx
@@ -23,7 +23,7 @@ const MobileItem: React.FC<MobileItemProps> = ({
   return (
     <Link
       href={href}
-      onClick={onClick}
+      onClick={handleClick}
       className={clsx(
         "group flex gap-x-3 text-sm leading-6 font-semibold w-full justify-center p-4 text-gray-500 hover:text-black hover:bg-gray-100",
         active && "bg-gray-100 text-black"

ab611a9d9fc5d7428e171bfadd8741460167e81b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-08 17:13:25 +0000

    message actions created

---
+++ app/actions/get-conversation-by-id.ts
@@ -0,0 +1,25 @@
+import getCurrentUser from "@/app/actions/get-current-user";
+import prisma from "@/app/libs/prismadb";
+
+const getConversationById = async (conversationId: string) => {
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser?.email) return null;
+
+    const conversation = await prisma.conversation.findUnique({
+      where: {
+        id: conversationId,
+      },
+      include: {
+        users: true,
+      },
+    });
+
+    return conversation;
+  } catch (error: unknown) {
+    return null;
+  }
+};
+
+export default getConversationById;

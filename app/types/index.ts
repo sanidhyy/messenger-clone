88cdf0d494fcc31f0812863e576495320d1fbed4
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-08 16:23:16 +0000

    types fix

---
+++ app/types/index.ts
@@ -0,0 +1,11 @@
+import type { Conversation, Message, User } from "@prisma/client";
+
+export type FullMessageType = Message & {
+  sender: User;
+  seen: User[];
+};
+
+export type FullConversationType = Conversation & {
+  users: User[];
+  messages: FullMessageType[];
+};

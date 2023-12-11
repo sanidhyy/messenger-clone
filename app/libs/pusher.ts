6649c57ef3cc12c8f9a8891cd8cdeb39122409b7
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-11 10:18:27 +0000

    pusher lib created

---
+++ app/libs/pusher.ts
@@ -0,0 +1,17 @@
+import PusherServer from "pusher";
+import PusherClient from "pusher-js";
+
+export const pusherServer = new PusherServer({
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+  secret: process.env.PUSHER_APP_SECRET!,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER!,
+  useTLS: true,
+});
+
+export const pusherClient = new PusherClient(
+  process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+  {
+    cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER!,
+  }
+);

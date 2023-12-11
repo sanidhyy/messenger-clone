c5cb0e63b2b81bd8f2451df9be2f8f88bf5daba5
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-11 12:13:39 +0000

    type error fix

--- app/actions/get-session.ts
+++ app/actions/get-session.ts
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/config/authOptions";
 
 export default async function getSession() {
-    return await getServerSession(authOptions)
+  return await getServerSession(authOptions);
 }

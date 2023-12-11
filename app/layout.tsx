afaabfeb016475241ec7642c414d65288929dcff
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-11 12:27:54 +0000

    site data updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,59 +1,21 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import type { PropsWithChildren } from "react";
 import { Inter } from "next/font/google";
 
 import ActiveStatus from "@/app/components/active-status";
 import ToasterContext from "@/app/context/toaster-context";
 import AuthContext from "@/app/context/auth-context";
+import { siteConfig } from "@/app/config/site";
 
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Messenger Clone",
-  description: "A Real-Time Messenger Clone using Next.js 14.",
-  keywords: [
-    "reactjs",
-    "nextjs",
-    "vercel",
-    "react",
-    "messenger clone",
-    "chat app",
-    "realtime",
-    "next-auth",
-    "zustand",
-    "socket.io",
-    "clsx",
-    "react-hot-toast",
-    "next-cloudinary",
-    "headlessui",
-    "nosql",
-    "bcrypt",
-    "mongodb",
-    "prisma",
-    "react-icons",
-    "react-select",
-    "react-spinners",
-    "axios",
-    "postcss",
-    "prettier",
-    "react-dom",
-    "tailwindcss",
-    "tailwindcss-animate",
-    "ui/ux",
-    "js",
-    "javascript",
-    "typescript",
-    "eslint",
-    "html",
-    "css",
-  ],
-  authors: {
-    name: "Sanidhya Kumar Verma",
-    url: "https://github.com/sanidhyy",
-  },
+export const viewport: Viewport = {
+  themeColor: "#0284C7",
 };
+
+export const metadata: Metadata = siteConfig;
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
